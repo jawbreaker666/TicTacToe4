6d3a1359bfa366afdbf1815b5b8db4d66bd2ddca
jawbreaker666 <andrasev96@web.de>
2021-12-02 10:43:22 +0100

    winner

--- script.js
+++ script.js
@@ -4,13 +4,96 @@ let currentShape = 'cross';
 
 
 function fillShape(id){
+    if(!fields[id]){
+
+    
     if(currentShape == 'cross'){
         currentShape = 'circle';
+        document.getElementById('player-1').classList.remove('player-inactive');
+        document.getElementById('player-2').classList.add('player-inactive');
     }
     else{
         currentShape = 'cross';
+        document.getElementById('player-1').classList.add('player-inactive');
+        document.getElementById('player-2').classList.remove('player-inactive');
+        
     }
 
-fields[id] = 'currentShape';
-console.log(fields);
-}
+    fields[id] = currentShape;
+    console.log(fields);
+    draw();
+    checkforwin();
+    }}
+
+function draw() {
+    for (let i = 0; i < fields.length; i++) {
+        if (fields[i] == 'circle') {
+        
+            document.getElementById('circle-' + i).classList.remove('d-none');
+            
+        
+        
+    }
+
+    
+        if (fields[i] == 'cross') {
+        
+            document.getElementById('cross-' + i).classList.remove('d-none');
+
+        }}}
+
+
+        function checkforwin() {
+        let winner;
+
+            if (fields[0] == fields[1] && fields[1] == fields[2] && fields[0]) {
+                winner = fields[0];
+        }
+    
+            if (fields[4] == fields[5] && fields[5] == fields[6] && fields[4]) {
+                winner = fields[4];
+        }
+    
+            if (fields[7] == fields[8] && fields[8] == fields[9] && fields[7]) {
+                winner = fields[7];
+
+        }
+            if (fields[0] == fields[3] && fields[3] == fields[6] && fields[0]) {
+                winner = fields[0];
+
+        }
+    
+            if (fields[1] == fields[4] && fields[4] == fields[7] && fields[1]) {
+                winner = fields[1];
+
+        }
+    
+            if (fields[2] == fields[5] && fields[5] == fields[8] && fields[2]) {
+                winner = fields[2];
+
+        }
+    
+            if (fields[0] == fields[4] && fields[4] == fields[8] && fields[0]) {
+                winner = fields[0];
+        }
+    
+            if (fields[2] == fields[4] && fields[4] == fields[6] && fields[2]) {
+                winner = fields[2];
+
+
+        }
+
+            if (!!winner){
+
+                console.log('GEWONNEN:', winner);
+        
+        }}
+    
+
+    
+    
+    
+    
+    
+    
+    
