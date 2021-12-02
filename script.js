ae8b39ccd44d10c2603f6aafafb7c18f3a9389c0
jawbreaker666 <andrasev96@web.de>
2021-12-02 11:58:17 +0100

    lines

--- script.js
+++ script.js
@@ -48,37 +48,45 @@ function draw() {
 
             if (fields[0] == fields[1] && fields[1] == fields[2] && fields[0]) {
                 winner = fields[0];
+                document.getElementById('line-1').style.transform = 'scaleX(1.3)';
         }
     
-            if (fields[4] == fields[5] && fields[5] == fields[6] && fields[4]) {
+            if (fields[3] == fields[4] && fields[4] == fields[5] && fields[3]) {
                 winner = fields[4];
+                document.getElementById('line-2').style.transform = 'scaleX(1.3)';
         }
     
             if (fields[7] == fields[8] && fields[8] == fields[9] && fields[7]) {
                 winner = fields[7];
+                document.getElementById('line-3').style.transform = 'scaleX(1.3)';
 
         }
             if (fields[0] == fields[3] && fields[3] == fields[6] && fields[0]) {
                 winner = fields[0];
+                document.getElementById('line-4').style.transform = 'rotate(90deg) scaleX(1.3)';
 
         }
     
             if (fields[1] == fields[4] && fields[4] == fields[7] && fields[1]) {
                 winner = fields[1];
+                document.getElementById('line-5').style.transform = 'rotate(90deg) scaleX(1.3)';
 
         }
     
             if (fields[2] == fields[5] && fields[5] == fields[8] && fields[2]) {
                 winner = fields[2];
+                document.getElementById('line-6').style.transform = 'rotate(90deg) scaleX(1.3)';
 
         }
     
             if (fields[0] == fields[4] && fields[4] == fields[8] && fields[0]) {
                 winner = fields[0];
+                document.getElementById('line-7').style.transform = 'rotate(45deg) scaleX(1.3)';
         }
     
             if (fields[2] == fields[4] && fields[4] == fields[6] && fields[2]) {
                 winner = fields[2];
+                document.getElementById('line-8').style.transform = 'rotate(-45deg) scaleX(1.3)';
 
 
         }
